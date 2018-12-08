7c5d3f3228a0609bad8d9c932be02bc361570b8c
Abhishek Yadav <zerothabhishek@gmail.com>
2018-12-08 12:26:29 +0530

    Trying functions: hello world

---
+++ functions/hello.js
@@ -0,0 +1,10 @@
+exports.handler = function(event, context, callback) {
+
+  console.log("Hello World");
+  console.log(event);
+
+  callback(null, {
+    statusCode: 200,
+    body: "Hello, World"
+  });
+}
