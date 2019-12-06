e5287487f8e002ef00f10295ce23512ab00dbc69
Abhishek Yadav <zerothabhishek@gmail.com>
2019-12-06 11:16:52 +0530

    Dec 2019 meetup

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,7 +10,7 @@ const Home = () => (
     <h3>Welcome to</h3>
     <h2>The Javascript Community in Chennai</h2>
     Next meetup: {'  '}
-    <a className="externalLink" href="https://www.meetup.com/meetup-group-zZPQtwTs/events/265929249/" target="_blank">
+    <a className="externalLink" href="https://www.meetup.com/meetup-group-zZPQtwTs/events/266751237/" target="_blank">
       Nov 2
     </a>
 
